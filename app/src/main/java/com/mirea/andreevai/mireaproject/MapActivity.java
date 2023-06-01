8ba39f69d377d4e7e4d5169654faa35da91513f9
Alexander Andreev <al29.andreev@gmail.com>
2023-06-01 20:31:45 +0300

    8 practice

---
+++ app/src/main/java/com/mirea/andreevai/mireaproject/MapActivity.java
@@ -0,0 +1,147 @@
+package com.mirea.andreevai.mireaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.PreferenceManager;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import com.mirea.andreevai.mireaproject.databinding.ActivityMapBinding;
+
+public class MapActivity extends AppCompatActivity {
+    private MapView mapView = null;
+    MyLocationNewOverlay locationNewOverlay;
+    private static final int REQUEST_CODE_PERMISSION = 100;
+    private boolean isWork = false;
+    private ActivityMapBinding  binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        binding = ActivityMapBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mapView = binding.mapView;
+        mapView.setZoomRounding(true);
+        mapView.setMultiTouchControls(true);
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(11.0);
+        GeoPoint startPoint = new GeoPoint(55.794229, 37.700772);
+        mapController.setCenter(startPoint);
+
+        int loc1PermissionStatus = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        int loc2PermissionStatus = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        int loc3PermissionStatus = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+
+        if (loc1PermissionStatus == PackageManager.PERMISSION_GRANTED && loc2PermissionStatus
+                == PackageManager.PERMISSION_GRANTED) {
+            isWork = true;
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE_PERMISSION);
+        }
+
+        CompassOverlay compassOverlay = new CompassOverlay(getApplicationContext(), new
+                InternalCompassOrientationProvider(getApplicationContext()), mapView);
+        compassOverlay.enableCompass();
+        mapView.getOverlays().add(compassOverlay);
+
+        final Context context = this.getApplicationContext();
+        final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        ScaleBarOverlay scaleBarOverlay = new ScaleBarOverlay(mapView);
+        scaleBarOverlay.setCentred(true);
+        scaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        mapView.getOverlays().add(scaleBarOverlay);
+
+        if (isWork) {
+
+            locationNewOverlay = new MyLocationNewOverlay(new
+                    GpsMyLocationProvider(getApplicationContext()), mapView);
+            locationNewOverlay.enableMyLocation();
+            mapView.getOverlays().add(this.locationNewOverlay);
+        }
+
+
+        Marker marker = new Marker(mapView);
+        marker.setPosition(new GeoPoint(55.749454, 37.583206));
+        marker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"NICEPRICE CAFE на Смоленской",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker);
+        marker.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+        marker.setTitle("Title");
+
+        Marker marker1 = new Marker(mapView);
+        marker1.setPosition(new GeoPoint(55.794245, 37.702039));
+        marker1.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"Кампус РТУ МИРЭА на Стромынке :)",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker1);
+        marker1.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+        marker1.setTitle("Title");
+
+        Marker marker2 = new Marker(mapView);
+        marker2.setPosition(new GeoPoint(55.783446, 37.721296));
+        marker2.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"NICEPRICE CAFE на Семёновской",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker2);
+        marker2.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+        marker2.setTitle("Title");
+
+
+
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView != null) {
+            mapView.onResume();
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Configuration.getInstance().save(getApplicationContext(),
+
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+
+        if (mapView != null) {
+            mapView.onPause();
+        }
+    }
+}
